9a6db5ed02a411a27b14fb5bf823e7065fabdb45
SpookyBitchShow <spookybitchshow@web.de>
2025-09-13 17:37:48 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'spookybitchshow.de',
+  base: '/',
   integrations: [
     compress({
       CSS: true,
